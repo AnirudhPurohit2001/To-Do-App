b24d39cea473e0d2a130abc083385aedab17118a
Anirudh Purohit <ani.purohit2001@gmail.com>
2025-02-14 23:29:27 +0530

    added implementation

--- src/components/AddToDo.tsx
+++ src/components/AddToDo.tsx
@@ -1,15 +1,27 @@
-import { useState } from "react"
+import { FormEvent, useState } from "react"
+import { useTodos } from "../store/todos";
 
 const AddToDo = () => {
+    const [todo, setTodo] = useState("");
+    const { handleAddToDo } = useTodos();
 
-    const [todo, setToDO] = useState("");
+    console.log("Brfore defining handleFormSubmit");
+
+    const handleFormSubmit = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log("Form Submitted with", todo);
+        handleAddToDo(todo);
+        setTodo("");
+    };
+
+    console.log("handleFormSubmit is defined:", handleFormSubmit);
 
     return (
         <form onSubmit={handleFormSubmit}>
-            <input type="text" value={todo} onChange={(e) => setToDO(e.target.value)} />
-            <button type="submit">ADD</button>
+            <input type="text" value={todo} onChange={(e) => setTodo(e.target.value)} />
+            <button type="submit">Add</button>
         </form>
-    )
-}
+    );
+};
 
 export default AddToDo
--- src/store/todos.tsx
+++ src/store/todos.tsx
@@ -1,27 +1,58 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState, useContext } from "react";
 
 export type TodosProviderProps = {
     children: ReactNode
 }
-export type TodosContext = {
-    todos: Todo[];
-    handleAddToDo: (task: string) => void;
-}
-
 export type Todo = {
     id: string;
     task: string;
     completed: boolean;
     created: Date;
 }
+export type TodosContext = {
+    todos: Todo[];
+    handleAddToDo: (task: string) => void; // Call Signature
+}
 
 
-export const todosContext = createContext(null)
+
+
+export const todosContext = createContext<TodosContext | null>(null)
 
 export const TodosProvider = ({ children }: TodosProviderProps) => {
-    return
-    <todosContext.Provider value={{ todo, handleAddToDo }}>
-        {children}
-    </todosContext.Provider>
 
+    const [todos, setTodos] = useState<Todo[]>([])
+
+    const handleAddToDo = (task: string) => {
+
+        setTodos((prev) => {
+            const newTodos: Todo[] = [
+                {
+                    id: Math.random().toString(),
+                    task: task,
+                    completed: false,
+                    created: new Date()
+                },
+                ...prev
+            ]
+            console.log(prev);
+            console.log(newTodos);
+            return newTodos
+        })
+    }
+
+    return < todosContext.Provider value={{ todos, handleAddToDo }}>
+        {children}
+    </todosContext.Provider >
+
+}
+
+//Consumer
+export const useTodos = () => {
+    const todosConsumer = useContext(todosContext);
+    if (!todosConsumer) {
+        throw new Error("useTodos used outside of provider")
+    }
+
+    return todosConsumer;
 }
