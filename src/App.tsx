ff736ee33bd0953c63f6d3070a9d74d3fa1e6466
Anirudh Purohit <ani.purohit2001@gmail.com>
2025-02-20 22:44:10 +0530

    added styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import AddToDo from "./components/AddToDo"
 import Navbar from "./components/Navbar"
 import Todos from "./components/Todos"
 import './App.css'
+import './index.css'
 
 const App = () => {
   return (
     <main>
-      <h1> TODO APPLICATION</h1>
+      <h1 className="text-6xl p-3 text-center"> TODO APPLICATION</h1>
       <Navbar />
       <AddToDo />
       <Todos />
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     let todos̥Data = searchParams.get("todos");
 
     return (
-        <nav>
+        <nav className='w-[55rem] flex justify-between items-center border-b border-gray-300 mx-auto mt-10'>
             <Link to="/" className={todos̥Data === null ? "active" : ""}>All</Link>
             <Link to="/?todos=active" className={todos̥Data === "active" ? "active" : ""}>Active</Link>
             <Link to="/?todos=completed" className={todos̥Data === "completed" ? "active" : ""}>Completed</Link>
